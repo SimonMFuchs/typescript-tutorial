6c60b3fa1515b40dce4e356b6449ec62defa4f9b
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-05-21 01:34:59 +0200

    Add test arrays with different types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'typescript-tutorial';
+
+  theArray: (number | string | boolean) [] = [1, 2, true, "hey"];  
+  theArray2: undefined [] = [];
+  // theArray3: number & string [] = [1, "hey"]; 
+  theArray4: null[] = [];
+
+  constructor() {
+    this.theArray = [1,"4"];
+    this.add(1,2);
+  }
+
+  // Parameter  definieren und aber auch den Rückgabewert der Funktion
+  add(a:number, b:number):number {
+    return a + b;
+  }
 }
